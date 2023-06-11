687172ab9a696ebac85ea27e1371d8dd59ff9aca
CLII412 <sw412fr@gmail.com>
2023-06-11 23:25:35 +0300

    init

--- index.js
+++ index.js
@@ -1,324 +1,124 @@
-//Задача 1: Напишіть функцію, яка перевіряє, чи є задане число парним. Використовуйте оператор залишку від ділення для визначення, чи є число парним.
+// Задача 1: Напишіть функцію, яка перетворює рядок в число, використовуючи Number()
 
-function isEvenNumber(num) {
-  // Перевірити, чи число num є парним
-  // Повернути результат перевірки (true або false) як результат функції
-  return num % 2 === 0;
+function stringToNumber(str) {
+  // Використовуємо Number() для перетворення рядка в число
+  // Повертаємо отримане число
 }
 
 console.log("Завдання 1 ====================================");
-console.log("isEvenNumber(10)", isEvenNumber(10)); // Виведе: true
-console.log("isEvenNumber(7)", isEvenNumber(7)); // Виведе: false
+console.log('stringToNumber("42")', stringToNumber("42")); // Виведе 42
 
-//Задача 2: Напишіть функцію, яка обчислює суму чисел від 1 до заданого числа. Використовуйте цикл для послідовного додавання чисел.
+// Задача 2: Напишіть функцію, яка визначає, чи є два числа майже однаковими, використовуючи Number.EPSILON
 
-function calculateSum(n) {
-  // Ініціалізувати змінну sum зі значенням 0
-  let sum = 0;
-
-  // Запустити цикл для змінної i від 1 до n
-  for (let i = 1; i <= n; i++) {
-    // Додати поточне значення i до змінної sum
-    sum += i;
-  }
-
-  // Повернути значення змінної sum
-  return sum;
+function isAlmostSame(num1, num2) {
+  // Визначаємо, чи є різниця між двома числами меншою або рівною EPSILON
 }
 
 console.log("Завдання 2 ====================================");
-console.log("calculateSum(5)", calculateSum(5)); // Виведе: 15
-console.log("calculateSum(10)", calculateSum(10)); // Виведе: 55
+console.log("isAlmostSame(0.1 + 0.2, 0.3)", isAlmostSame(0.1 + 0.2, 0.3)); // Виведе true
 
-//Задача 3: Напишіть функцію, яка перевіряє, чи є задане число простим(ділиться без остачі тільки на себе та на 1). Використовуйте цикл для перевірки ділення числа на всі можливі дільники.
+// Задача 3: Напишіть функцію, яка перевіряє, чи є число безпечним цілим числом
 
-function isPrimeNumber(num) {
-  if (num <= 1) {
-    return false;
-  }
-  for (let i = 2; i < num; i++) {
-    if (num % i === 0) {
-      return false;
-    }
-  }
-  return true;
+function isSafeInteger(num) {
+  // Перевіряємо, чи є число меншим або рівним MAX_SAFE_INTEGER і більшим або рівним MIN_SAFE_INTEGER
 }
 
 console.log("Завдання 3 ====================================");
-console.log("isPrimeNumber(7)", isPrimeNumber(7)); // Виведе: true
-console.log("isPrimeNumber(10)", isPrimeNumber(10)); // Виведе: false
+console.log(
+  "isSafeInteger(Number.MAX_SAFE_INTEGER + 1)",
+  isSafeInteger(Number.MAX_SAFE_INTEGER + 1)
+); // Виведе false
 
-//Задача 4: Напишіть функцію, яка перевіряє, чи є задане число додатним. Використовуйте умовний оператор для перевірки числа.
+// Задача 4: Використовуючи результати Задачі 3, напишіть функцію, яка перевіряє, чи є число небезпечним цілим числом
 
-function isPositiveNumber(num) {
-  return num > 0;
+function isUnsafeInteger(num) {
+  // Використовуємо логічну оператор НЕ (!), щоб отримати протилежну відповідь від функції isSafeInteger()
+}
+
+console.log("Завдання 4 ====================================");
+console.log(
+  "isUnsafeInteger(Number.MIN_SAFE_INTEGER - 1)",
+  isUnsafeInteger(Number.MIN_SAFE_INTEGER - 1)
+); // Виведе true
+
+// Задача 5: Напишіть функцію, яка перевіряє, чи є число надто великим для представлення в JavaScript
+
+function isTooLarge(num) {
+  // Перевіряємо, чи є число більшим за MAX_VALUE
 }
 
 console.log("Завдання 5 ====================================");
-console.log("isPositiveNumber(5)", isPositiveNumber(5)); // Виведе: true
-console.log("isPositiveNumber(-2)", isPositiveNumber(-2)); // Виведе: false
+console.log(
+  "isTooLarge(Number.MAX_VALUE * 2)",
+  isTooLarge(Number.MAX_VALUE * 2)
+); // Виведе true
 
-//Задача 5: Напишіть функцію, яка перевіряє, чи є заданий рядок пустим. Використовуйте умовний оператор та властивість довжини рядка для перевірки.
+// Задача 6: Напишіть функцію, яка перевіряє, чи є число надто малим для представлення в JavaScript
 
-function isEmptyString(str) {
-  return str === "";
+function isTooSmall(num) {
+  // Перевіряємо, чи є число більшим за 0, але меншим за MIN_VALUE
 }
 
-console.log("Завдання 5 ====================================");
-console.log('isEmptyString("")', isEmptyString("")); // Виведе: true
-console.log('isEmptyString("Hello")', isEmptyString("Hello")); // Виведе: false
+// Задача 6: Напишіть функцію, яка перевіряє, чи є число близьким до 0, але все ще більшим за Number.MIN_VALUE
 
-//Задача 6: Напишіть функцію checkDataType, яка приймає параметр value і перевіряє його тип даних. Функція повинна використовувати оператор typeof для визначення типу значення.
-// Виведіть в консоль повідомлення з результатом перевірки типу, наприклад, Значення "hello" має тип "string"
-
-function checkDataType(value) {
-  const dataType = typeof value;
-  console.log(`Значення "${value}" має тип "${dataType}".`);
-  return dataType;
+function isAlmostZero(num) {
+  // Перевіряємо, чи є число близьким до 0, але все ще більшим за Number.MIN_VALUE
 }
-
-// Приклади виклику функції
 
 console.log("Завдання 6 ====================================");
-console.log("checkDataType(42)", checkDataType(42)); // Виведе: Значення "42" має тип "number".
-console.log("checkDataType('Hello')", checkDataType("Hello")); // Виведе: Значення "Hello" має тип "string".
-console.log("checkDataType(true)", checkDataType(true)); // Виведе: Значення "true" має тип "boolean".
-console.log("checkDataType(undefined)", checkDataType(undefined)); // Виведе: Значення "undefined" має тип "undefined".
+console.log(
+  "isAlmostZero(Number.MIN_VALUE / 2)",
+  isAlmostZero(Number.MIN_VALUE / 2)
+); // Виведе false
+console.log("isAlmostZero(Number.MIN_VALUE)", isAlmostZero(Number.MIN_VALUE)); // Виведе true
 
-//Задача 7: Напишіть функцію findMax, яка приймає два числа num1 і num2 і повертає більше з них. Використовуйте умовний оператор if та оператор порівняння для визначення більшого числа.
-
-function findMax(num1, num2) {
-  if (num1 > num2) {
-    return num1;
-  } else {
-    return num2;
-  }
-}
-
-// Приклади виклику функції
 console.log("Завдання 7 ====================================");
-console.log("findMax(10, 5)", findMax(10, 5)); // Виведе: 10
-console.log("findMax(8,12)", findMax(8, 12)); // Виведе: 12
-console.log("findMax(7, 7)", findMax(7, 7)); // Виведе: 7
+console.log("checkIsInteger(42.5)", checkIsInteger(42.5)); // Виведе false
 
-//Задача 8:  Напишіть функцію getUserStatus, яка приймає вік користувача age і повертає рядок, що вказує на його статус.
-// Якщо вік менше 18 років, функція повертає "Неповнолітній". Якщо вік від 18 до 65 років, функція повертає "Дорослий". Якщо вік більше або дорівнює 65 рокам, функція повертає "Пенсіонер".
-// Використовуйте умовний оператор if, else if та оператори порівняння для визначення статусу користувача.
+// Задача 8: Напишіть функцію, яка перевіряє, чи є значення безпечним цілим числом
 
-function getUserStatus(age) {
-  if (age < 18) {
-    return "Неповнолітній";
-  } else if (age >= 18 && age < 65) {
-    return "Дорослий";
-  } else {
-    return "Пенсіонер";
-  }
+function checkIsSafeInteger(num) {
+  // Використовуємо вбудовану функцію Number.isSafeInteger(), щоб перевірити, чи є значення безпечним цілим числом
 }
 
-// Приклади виклику функції
 console.log("Завдання 8 ====================================");
-console.log("getUserStatus(15)", getUserStatus(15)); // Виведе: "Неповнолітній"
-console.log("getUserStatus(25)", getUserStatus(25)); // Виведе: "Дорослий"
-console.log("getUserStatus(70)", getUserStatus(70)); // Виведе: "Пенсіонер"
+console.log(
+  "checkIsSafeInteger(Math.pow(2, 53))",
+  checkIsSafeInteger(Math.pow(2, 53))
+); // Виведе false
 
-//Задача 9:  Напишіть функцію getDayOfWeek, яка приймає номер дня тижня dayNumber (1-7) і повертає рядок, що відповідає цьому дню тижня.
-// Використовуйте конструкцію switch case для визначення дня тижня за його номером.
+// Задача 9: Напишіть функцію, яка конвертує число в рядок з експоненційним представленням
 
-function getDayOfWeek(dayNumber) {
-  let day;
-
-  switch (dayNumber) {
-    case 1:
-      day = "Понеділок";
-      break;
-    case 2:
-      day = "Вівторок";
-      break;
-    case 3:
-      day = "Середа";
-      break;
-    case 4:
-      day = "Четвер";
-      break;
-    case 5:
-      day = "П'ятниця";
-      break;
-    case 6:
-      day = "Субота";
-      break;
-    case 7:
-      day = "Неділя";
-      break;
-    default:
-      day = "Невірний номер дня";
-  }
-
-  return day;
+function convertToExponential(num) {
+  // Використовуємо метод toExponential(), щоб конвертувати число в рядок з експоненційним представленням
 }
 
-// Приклади виклику функції
 console.log("Завдання 9 ====================================");
-console.log("getDayOfWeek(1)", getDayOfWeek(1)); // Виведе: "Понеділок"
-console.log("getDayOfWeek(3)", getDayOfWeek(3)); // Виведе: "Середа"
-console.log("getDayOfWeek(6)", getDayOfWeek(6)); // Виведе: "Субота"
-console.log("getDayOfWeek(8)", getDayOfWeek(8)); // Виведе: "Невірний номер дня"
+console.log("convertToExponential(42)", convertToExponential(42)); // Виведе "4.2e+1"
 
-//Задача 10: Напишіть функцію getVariableType, яка приймає змінну variable і повертає рядок, що відповідає типу цієї змінної. Використовуйте тернарний оператор для визначення типу змінної.
+// Задача 10: Напишіть функцію, яка конвертує число в рядок з фіксованою кількістю знаків після коми
 
-function getVariableType(variable) {
-  const type = typeof variable;
-  const result =
-    type === "number"
-      ? "Це число"
-      : type === "string"
-      ? "Це рядок"
-      : type === "boolean"
-      ? "Це булеве значення"
-      : "Це інший тип";
-
-  return result;
+function convertToFixed(num, precision) {
+  // Використовуємо метод toFixed(), щоб конвертувати число в рядок з фіксованою кількістю знаків після коми
 }
 
-// Приклади виклику функції
 console.log("Завдання 10 ====================================");
-console.log("getVariableType(10)", getVariableType(10)); // Виведе: "Це число"
-console.log('getVariableType("Hello")', getVariableType("Hello")); // Виведе: "Це рядок"
-console.log("getVariableType(true)", getVariableType(true)); // Виведе: "Це булеве значення"
-console.log("getVariableType([1, 2, 3])", getVariableType([1, 2, 3])); // Виведе: "Це інший тип"
+console.log("convertToFixed(42.9876, 2)", convertToFixed(42.9876, 2)); // Виведе "42.99"
 
-//Задача 11: Напишіть рекурсивну функцію, яка обчислює суму чисел від 1 до заданого числа.
-// Факторіал числа - це добуток всіх цілих чисел від 1 до цього числа. Наприклад, факторіал 5 (записується як 5!) дорівнює 1*2*3*4*5 = 120.
+// Задача 11: Напишіть функцію, яка конвертує число в рядок
 
-function factorial(n) {
-  // Якщо n дорівнює 0 або 1, повертаємо 1, оскільки факторіал 0 і 1 завжди дорівнює 1.
-  if (n === 0 || n === 1) {
-    return 1;
-  }
-  // Якщо n більше за 1, ми множимо n на результат виклику тієї самої функції з n-1.
-  else {
-    return n * factorial(n - 1);
-  }
+function convertToString(num) {
+  // Використовуємо метод toString(), щоб конвертувати число в рядок
 }
 
 console.log("Завдання 11 ====================================");
-console.log("factorial(5)", factorial(5)); // Виведе: 120
-console.log("factorial(10)", factorial(10)); // Виведе: 3628800
+console.log("convertToString(42)", convertToString(42)); // Виведе "42"
 
-// Задача 12: створити функцію `makeAdder`, яка приймає число `x` і повертає нову функцію, що приймає число `y` і повертає суму `x` та `y`.
-function makeAdder(x) {
-  // Вертаємо нову функцію, яка приймає `y`
-  return function (y) {
-    // Сумуємо `x` та `y` і повертаємо результат
-    return x + y;
-  };
+// Задача 12: Напишіть функцію, яка конвертує число в рядок з вказаною загальною кількістю значущих цифр
+
+function convertToPrecision(num, precision) {
+  // Використовуємо метод toPrecision(), щоб конвертувати число в рядок з вказаною загальною кількістю значущих цифр
 }
-
-//Створюємо змінну getAdder якій присвоємо результат виконання функції makeAdder з аргументом 5
-let getAdder = makeAdder(5);
 
 console.log("Завдання 12 ====================================");
-console.log("getAdder(10)", getAdder(10)); // Виведе: 15
-console.log("makeAdder(5)(10)", makeAdder(5)(10)); // Виведе: 15
-
-//Задача 13: Задача: створити стрілкову функцію, яка приймає два числа і повертає їх добуток.
-let multiply = (x, y) => {
-  // Повертаємо добуток x та y
-  return x * y;
-};
-
-console.log("Завдання 13 ====================================");
-console.log("multiply(5, 3)", multiply(5, 3)); // Виведе: 15
-
-//Задача 14: Задача: створити каріровану функцію `divide`, яка може бути викликана як `divide(x)(y)`, щоб отримати результат ділення `x` на `y`.
-
-function divide(x) {
-  // Повертаємо нову функцію, яка приймає `y`
-  return function (y) {
-    // Ділимо `x` на `y` і повертаємо результат
-    return x / y;
-  };
-}
-
-//Створюємо змінну getDivider якій присвоємо результат виконання функції divide з аргументом 2
-let getDivider = divide(2);
-
-console.log("Завдання 14 ====================================");
-console.log("getDivider(10)", getDivider(10)); // Виведе: 0.2
-console.log("divide(2)(10)", divide(2)(10)); // Виведе: 0.2
-
-//Задача 15: Створити мемоізовану версію функції, яка обчислює площу квадрата на основі довжини його сторони.
-
-function memoizedArea() {
-  // Ініціалізуємо примітивні змінні для зберігання попереднього аргументу та результату
-  let prevSide = null;
-  let prevResult = null;
-
-  // Повертаємо функцію яка приймає аргумент side - сторону квадрата
-  return function (side) {
-    // Перевіряємо, чи є введена сторона такою ж, як і в попередньому виклику
-    if (side === prevSide) {
-      // Виводимо повідомлення Fetching from cache
-      console.log("Fetching from cache");
-      // Повертаємо попереднє значення
-      return prevResult;
-    }
-    // Якщо сторона відрізняється від попередньої, обчислюємо площу та зберігаємо результат та аргумент для майбутнього використання
-    else {
-      // Виводимо повідомлення Calculating result
-      console.log("Calculating result");
-      //Розраховуємо площу
-      let result = side * side;
-
-      //prevSide присвоюємо сторону квадрата
-      prevSide = side;
-      //prevResult просвоюємо площу квадрата
-      prevResult = result;
-      // Повертаємо площу
-      return result;
-    }
-  };
-}
-
-//Створюємо змінну squareArea якій присвоємо результат виконання функції memoizedArea
-let squareArea = memoizedArea();
-
-console.log("Завдання 15 ====================================");
-console.log("squareArea(5)", squareArea(5)); // Обчислює і виводить 25
-console.log("squareArea(5)", squareArea(5)); // Виводить "Fetching from cache" і виводить 25 з кешу
-console.log("squareArea(6)", squareArea(6)); // Обчислює і виводить 36
-console.log("squareArea(6)", squareArea(6)); // Виводить "Fetching from cache" і виводить 36 з кешу
-
-//Задача 16: Задача: створити функціональний вираз для обчислення кубу числа.
-let cube = function (n) {
-  // Повертаємо n в кубі
-  return n ** 3;
-};
-
-console.log("Завдання 16 ====================================");
-console.log("cube(3)", cube(3)); // Виведе: 27
-
-// Задача 17: створити дві функції, одна з яких збільшує число на 1, а інша - множить число на 2, а потім створити композицію цих функцій.
-
-function increment(n) {
-  // Повертаємо n + 1
-  return n + 1;
-}
-
-function double(n) {
-  // Повертаємо n*2
-  return n * 2;
-}
-
-function compose(func1, func2) {
-  // Повертаємо нову функцію, яка приймає число
-  return function (n) {
-    //Повертаємо func2, в яку передаємо func1 з аргументом n
-    return func2(func1(n));
-  };
-}
-
-//Створюємо змінну createCompose якій присвоємо результат виконання функції compose з аргументами increment та double
-let createCompose = compose(increment, double);
-
-console.log("Завдання 17 ====================================");
-console.log("createCompose(5)", createCompose(5)); // Виведе: 12 (5+1=6, 6*2=12)
+console.log("convertToPrecision(42.9876, 2)", convertToPrecision(42.9876, 2)); // Виведе "43"
