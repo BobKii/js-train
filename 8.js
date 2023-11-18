79e18ace0c10f5cd7e3814c7eb58a094f7d84818
BobKii <two.heap2008@gmail.com>
2023-11-18 22:11:45 +0200

    181123 PrePaid_1-8  train-24

--- 8.js
+++ 8.js
@@ -1,12 +1,26 @@
 console.log("Завдання: 8 ==============================");
 
 function task8() {
-  // Створюємо функцію fetchFakeData, яка симулює запит до сервера та повертає "дані" через Promise.resolve().
+  // Створюємо функцію fetchFakeData, яка симулює запит до сервера
+  // та повертає "дані" через Promise.resolve().
   // Створюємо fakeData константу з данними { name: "John",age: 30}
-  // Використовуємо Promise.resolve(fakeData) для того, щоб створити вже зарезолвлений проміс.
+  // Використовуємо Promise.resolve(fakeData) для того, щоб створити
+  // вже зарезолвлений проміс.
+  function fetchFakeData() {
+    const fakeData = { name: "John", age: 30 };
+    let result = Promise.resolve(fakeData);
+    console.log(result);
+    return result;
+  }
   // Викликаємо функцію fetchFakeData
-  // Якщо проміс виконаний успішно виводимо в консоль дані які він повертає
+  // Якщо проміс виконаний успішно виводимо в консоль дані
+  // які він повертає
+
   // Якщо проміс виконаний з помилкою виводимо в консоль помилку
+  fetchFakeData()
+    .then((value) => console.log(value))
+    .catch((Err) => console.log(Err))
+    .finally(() => console.log(`==  Виконання завдання 8 заверщено  ==`));
 }
 
 // Викликаємо функцію task8
